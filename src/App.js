283747cbeb564ed3be1c43ad3483ea48873f35d4
GOKUL-KANNAN-P <gokulkannanp.cse2021@citchennai.net>
2023-09-26 14:12:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import "./styles.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+import imag from "./icons/placeholder.png"
+
+import { Icon, divIcon, point } from "leaflet";
+
+// create custom icon
+const customIcon = new Icon({
+  // iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
+  iconUrl: imag,
+  iconSize: [38, 38] // size of the icon
+});
+
+// custom cluster icon
+const createClusterCustomIcon = function (cluster) {
+  return new divIcon({
+    html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
+    className: "custom-marker-cluster",
+    iconSize: point(33, 33, true)
+  });
+};
+
+// markers
+const markers = [
+  {
+    geocode: [12.971581, 80.043],
+    popUp: "Load : 450 Kg"
+  },
+  {
+    geocode: [12.9934, 80.085],
+    popUp: "Load : 100 kg"
+  },
+  {
+    geocode: [12.9603, 80.05827],
+    popUp: "Load : 200 kg"
+  },
+  {
+    geocode: [13.0499, 80.2095],
+    popUp: "Load : 200 kg"
+  },
+  {
+    geocode: [13.067439, 80.237617],
+    popUp: "Load : 200 kg"
+  },
+  {
+    geocode: [13.1009, 80.1902],
+    popUp: "Load : 200 kg"
+  },
+  {
+    geocode: [13.0355, 80.2305],
+    popUp: "Load : 200 kg"
+  },
+  {
+    geocode: [12.9763, 80.1593],
+    popUp: "Load : 200 kg"
+  },
+  {
+    geocode: [13.0689, 80.1968],
+    popUp: "Load : 300 kg"
+  }
+];
+
+export default function App() {
+  return (
+    <MapContainer center={[12.971581, 80.043]} zoom={13}>
+      {/* OPEN STREEN MAPS TILES */}
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* WATERCOLOR CUSTOM TILES */}
+      {/* <TileLayer
+        attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
+      /> */}
+      {/* GOOGLE MAPS TILES */}
+      {/* <TileLayer
+        attribution="Google Maps"
+        // url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
+        // url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
+        url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // terrain
+        maxZoom={20}
+        subdomains={["mt0", "mt1", "mt2", "mt3"]}
+      /> */}
+
+      <MarkerClusterGroup
+        chunkedLoading
+        iconCreateFunction={createClusterCustomIcon}
+      >
+        {/* Mapping through the markers */}
+        {markers.map((marker) => (
+          <Marker position={marker.geocode} icon={customIcon}>
+            <Popup>{marker.popUp}
+            <button className="button">
+              PRE-ORDER
+            </button></Popup>
+          </Marker>
+        ))}
+
+        {/* Hard coded markers */}
+        {/* <Marker position={[51.505, -0.09]} icon={customIcon}>
+          <Popup>This is popup 1</Popup>
+        </Marker>
+        <Marker position={[51.504, -0.1]} icon={customIcon}>
+          <Popup>This is popup 2</Popup>
+        </Marker>
+        <Marker position={[51.5, -0.09]} icon={customIcon}>
+          <Popup>This is popup 3</Popup>
+        </Marker>
+       */}
+      </MarkerClusterGroup>
+    </MapContainer>
+  );
+}
